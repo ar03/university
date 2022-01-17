52ff3d1f6ad3a8c11b7705e0e90d797b4756f6ae
ar03 <34195549+ar03@users.noreply.github.com>
2022-01-17 06:23:06 -0600

    add page and single | add posts

--- app/public/wp-content/themes/university-theme/page.php
+++ app/public/wp-content/themes/university-theme/page.php
@@ -2,9 +2,10 @@
 
     while(have_posts()) {
         the_post(); ?>
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <h1>page</h1>
+        <h2><?php the_title(); ?></h2>
         <?php the_content(); ?>
-        <hr>
+       
    <?php }
 
 ?>
