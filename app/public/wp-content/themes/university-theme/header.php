d4357258a56d24cbd7c41ad3afa43d33665d3345
ar03 <34195549+ar03@users.noreply.github.com>
2022-01-21 06:57:28 -0600

    add front-page.php | update blog

--- app/public/wp-content/themes/university-theme/header.php
+++ app/public/wp-content/themes/university-theme/header.php
@@ -16,11 +16,11 @@
         <div class="site-header__menu group">
           <nav class="main-navigation">
             <ul> <!-- is_page('about-us) = true  || helps stay active while on child; get_post_parent set to 0 will retrieve itself -->
-              <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 14) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us');?>">About Us</a></li>
+              <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 14) echo 'class="current-menu-item"' ?>><a href="<?=site_url('/about-us');?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
-              <li><a href="#">Blog</a></li>
+              <li><a href="<?= site_url('/blog'); ?>">Blog</a></li>
             </ul>
           </nav>
           <div class="site-header__util">
