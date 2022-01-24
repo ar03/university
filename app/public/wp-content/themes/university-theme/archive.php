2f8b0f3f01f3803972d15de49b0cf051ae3ee94f
ar03 <34195549+ar03@users.noreply.github.com>
2022-01-24 06:20:36 -0600

    update archive | function for links

--- app/public/wp-content/themes/university-theme/archive.php
+++ app/public/wp-content/themes/university-theme/archive.php
@@ -5,11 +5,7 @@ get_header(); ?>
 <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg');?>)"></div>
       <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">
-            <?php 
-                if (is_category()) { single_cat_title(); } 
-                if (is_author()) { echo 'Posts by '; the_author(); }
-            ?></h1>
+        <h1 class="page-banner__title"><?php the_archive_title(); ?></h1>
         <div class="page-banner__intro">
           <p>Keep up with our latest news</p>
         </div>
