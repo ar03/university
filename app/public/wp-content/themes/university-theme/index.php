b5d5c9d38d6cb628ba24d23bbeb6bca653b77bc5
ar03 <34195549+ar03@users.noreply.github.com>
2022-01-24 06:09:33 -0600

    add pagination | update single

--- app/public/wp-content/themes/university-theme/index.php
+++ app/public/wp-content/themes/university-theme/index.php
@@ -26,6 +26,7 @@ get_header(); ?>
                 </div>
             </div>
        <?php }
+       echo paginate_links();
     ?>
 </div>
 <?php get_footer();
--- app/public/wp-content/themes/university-theme/page.php
+++ app/public/wp-content/themes/university-theme/page.php
@@ -16,7 +16,7 @@
             $theParent = wp_get_post_parent_id(get_the_ID());
             if ($theParent) { ?>
                 <div class="metabox metabox--position-up metabox--with-home-link">
-                    <p><a class="metabox__blog-home-link" href="<?php get_permalink($theParent); ?>">
+                    <p><a class="metabox__blog-home-link" href="<?= get_permalink($theParent); ?>">
                         <i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent);?></a> 
                     <span class="metabox__main"><?php the_title(); ?></span></p>
                 </div>
