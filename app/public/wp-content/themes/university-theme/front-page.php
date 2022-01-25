b097af5cf1b85b98e57826fdffffab59b5281795
ar03 <34195549+ar03@users.noreply.github.com>
2022-01-25 06:51:42 -0600

    add custom/default excerpt

--- app/public/wp-content/themes/university-theme/front-page.php
+++ app/public/wp-content/themes/university-theme/front-page.php
@@ -57,7 +57,11 @@
                         <span class="event-summary__day"><?php the_time('d'); ?></span></a>
                       <div class="event-summary__content">
                         <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                        <p><?= wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+                        <p><?php if(has_excerpt()){ //evaluates to true if custom excerpt is present
+                          echo the_excerpt();
+                          } else {
+                            echo wp_trim_words(get_the_content(), 18);
+                            } ?><a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
                       </div>
                     </div>
               <?php } wp_reset_postdata();
