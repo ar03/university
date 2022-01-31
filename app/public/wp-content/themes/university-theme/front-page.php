e0232d3b10281f01dbf6e8e5acc9a234cfe597ae
ar03 <34195549+ar03@users.noreply.github.com>
2022-01-31 05:48:11 -0600

    add custom query for event ordering

--- app/public/wp-content/themes/university-theme/front-page.php
+++ app/public/wp-content/themes/university-theme/front-page.php
@@ -17,9 +17,21 @@
           <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
           <?php
-            $homepageEvents = new WP_Query(array (
-              'posts_per_page' => 2,
-              'post_type' => 'event'
+            $today = date('Ymd');
+            $homepageEvents = new WP_Query(array ( //Event query
+              'posts_per_page' => -1, // -1 returns all posts of that type
+              'post_type' => 'event',
+              'meta_key' => 'event_date',
+              'orderby' => 'meta_value_num',
+              'order' => 'ASC',
+              'meta_query' => array(
+                array(
+                  'key' => 'event_date',
+                  'compare' => '>=',
+                  'value' => $today,
+                  'type' => 'numeric'//helps WP compare numbers for this query
+                )
+              )
             ));
 
             while ($homepageEvents->have_posts()) {
