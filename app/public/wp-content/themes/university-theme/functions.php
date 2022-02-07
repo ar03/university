c9c0a0d9ac8ee4a2489c7ffea0dc4880946fc166
ar03 <34195549+ar03@users.noreply.github.com>
2022-02-07 06:36:44 -0600

    add dynamic background for page banner

--- app/public/wp-content/themes/university-theme/functions.php
+++ app/public/wp-content/themes/university-theme/functions.php
@@ -13,8 +13,9 @@ function university_features() {
     register_nav_menu('footerMenuTwo', 'Footer Menu Two');
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
-    add_image_size('professorLandscape', 400, 260, true);//3rd arg is cropping - starting at 'left' or 'right', 'top' or 'bottom'
+    add_image_size('professorLandscape', 400, 260, true);//3rd arg is cropping - starting at 'left' or 'right', 'top' or 'bottom', use plugin 'manuel image crop tomasz'
     add_image_size('professorPortrait', 480, 650, true);
+    add_image_size('pageBanner', 1500, 350, true);
 }
 
 add_action('after_setup_theme', 'university_features');
--- app/public/wp-content/themes/university-theme/single-professor.php
+++ app/public/wp-content/themes/university-theme/single-professor.php
@@ -4,11 +4,11 @@
         the_post(); ?>
 
 <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg');?>)"></div>
+      <div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImage = get_field('page_banner_background_image'); echo $pageBannerImage['sizes']['pageBanner'] ?>);"></div>
       <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title"><?php the_title(); ?></h1>
         <div class="page-banner__intro">
-          <p>Learn how the school of your dreams got started.</p>
+          <p><?php the_field('page_banner_subtitle'); ?></p>
         </div>
       </div>
     </div>
