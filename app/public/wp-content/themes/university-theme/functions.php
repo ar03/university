119b73e08ec7520a71347d7a041f033adc12eed0
ar03 <34195549+ar03@users.noreply.github.com>
2022-02-07 06:19:01 -0600

    add plugin for image cropping
    manual image cropping tomasz

--- app/public/wp-content/themes/university-theme/functions.php
+++ app/public/wp-content/themes/university-theme/functions.php
@@ -13,7 +13,7 @@ function university_features() {
     register_nav_menu('footerMenuTwo', 'Footer Menu Two');
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
-    add_image_size('professorLandscape', 400, 260, true);
+    add_image_size('professorLandscape', 400, 260, true);//3rd arg is cropping - starting at 'left' or 'right', 'top' or 'bottom'
     add_image_size('professorPortrait', 480, 650, true);
 }
 
