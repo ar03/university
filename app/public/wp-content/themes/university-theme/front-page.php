025d6a69ac4abd6547bc8cd3643d90829c9c1784
ar03 <34195549+ar03@users.noreply.github.com>
2022-02-08 06:25:25 -0600

    refactor with get_template_parts()

--- app/public/wp-content/themes/university-theme/front-page.php
+++ app/public/wp-content/themes/university-theme/front-page.php
@@ -35,28 +35,9 @@
             ));
 
             while ($homepageEvents->have_posts()) {
-              $homepageEvents->the_post(); ?>
-              <div class="event-summary">
-                <a class="event-summary__date t-center" href="#">
-                <span class="event-summary__month"><?php 
-                  $eventDate = new DateTime(get_field('event_date'));
-                  echo $eventDate->format('M')
-                ?></span>
-                <span class="event-summary__day"><?php 
-                  $eventDate = new DateTime(get_field('event_date'));
-                  echo $eventDate->format('d')
-                ?></span></a>
-
-                <div class="event-summary__content">
-                  <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                  <p><?php if(has_excerpt()){ //evaluates to true if custom excerpt is present
-                          echo the_excerpt();
-                          } else {
-                            echo wp_trim_words(get_the_content(), 18);
-                            } ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-                </div>
-              </div>
-           <?php }
+              $homepageEvents->the_post(); 
+              get_template_part('template-parts/content', 'event');//get_template_part('template-parts/content', get_post_type())
+            }                                                                         //file location          speciality name -professor, -event
           ?>
 
           <p class="t-center no-margin"><a href="<?php get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
