7d598301cb58ebae583c06e12a9e8ab62db9a1eb
ar03 <34195549+ar03@users.noreply.github.com>
2022-02-23 07:59:30 -0600

    add multiple post types to search

--- app/public/wp-content/themes/university-theme/includes/search-route.php
+++ app/public/wp-content/themes/university-theme/includes/search-route.php
@@ -9,21 +9,52 @@ function univeristyRegisterSearch() {
 }
 
 function universitySearchResults($data) {
-  $professors = new WP_Query(array(
-    'post_type' => 'professor',
-    's' => $data['term']
+  $mainQuery = new WP_Query(array(
+    'post_type' => array('post', 'page', 'professor', 'campus', 'event'),
+    's' => sanitize_text_field($data['term'])
   ));
 
-  $professorResults = array(); 
+  $results = array(
+    'generalInfo' => array(),
+    'professors' => array(),
+    'programs' => array(),
+    'events' => array(),
+    'campuses' => array()
+  ); 
   
-  while($professors->have_posts()) {
-    $professors->the_post();
-    array_push($professorResults, array(
-      'title' => get_the_title(),
-      'permalink' => get_the_permalink()
-    ));
+while($mainQuery->have_posts()) {
+    $mainQuery->the_post();
+    if (get_post_type() == 'post' OR get_post_type() == 'page') {
+        array_push($results['generalInfo'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink()
+        ));
+      }
+    if (get_post_type() == 'professor') {
+      array_push($results['professors'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ));
+    }
 
+    if (get_post_type() == 'program') {
+      array_push($results['programs'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ));
+    }
+    if (get_post_type() == 'event') {
+      array_push($results['events'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ));
+    }
+    if (get_post_type() == 'campus') {
+      array_push($results['campuses'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ));
+    }
   }
-
-  return $professorResults;
+  return $results;
 }
